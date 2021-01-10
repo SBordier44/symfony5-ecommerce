db1f0c2183cc70ec03b44ae241e19672d5eb60b0
Sylvain B <slye@nubox.fr>
2021-01-10 23:58:25 +0100

    CI & clean code checker

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
